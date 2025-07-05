03b778b78c32a48854e740bbdbf14ea6c90b2fe0
Sadaf-8 <sadafmoneeb8@gmail.com>
2025-07-05 13:55:36 +0500

    Initial commit: Add Task2 project

---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace CSharpConsoleAppWithUserInput
+{
+    class Program()
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Age: ");
+            int Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Height: ");
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Grade: ");
+            char Grade = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Student Status true/false: ");
+            bool IsStudent = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nName: {name}");
+            Console.WriteLine($"\nAge: {Age}");
+            Console.WriteLine($"\nHeight: {height}");
+            Console.WriteLine($"\nGrade: {Grade}");
+            Console.WriteLine($"\nStudent Status: {IsStudent}");
+
+
+        }
+    }
+}
